d13120589dae56c20118d829ca47d8f446610a40
meow <mingyeow@gmail.com>
2020-07-03 02:12:36 +0800

    break up basic into new files

---
+++ addToken.js
@@ -0,0 +1,21 @@
+require('dotenv').config()
+var FPR = require("kyber-fpr-sdk")
+var conversionRates = FPR.ConversionRatesContract
+var Web3 = require("web3")
+var addresses = require("./addresses.json")
+
+const tokenInfo = new conversionRates.TokenControlInfo(100000000000000,440000000000000000000n,920000000000000000000n)
+const KTTokenAddress = "0xc376079608C0F17FE06b9e950872666f9c3C3DA4"
+
+const provider = new Web3.providers.HttpProvider(process.env.ROPSTEN_NODE_URL)
+const web3 = new Web3(provider)
+const account = web3.eth.accounts.privateKeyToAccount(process.env.TESTNET_PRIVATE_KEY)
+web3.eth.accounts.wallet.add(account)
+
+const manageReserve = new FPR.Reserve(web3, addresses)
+const CRContract = new FPR.ConversionRatesContract(web3, addresses.conversionRates)
+
+manageReserve.addToken(account, KTTokenAddress, tokenInfo)
+   .then( (result) => {
+      console.log(result)
+   })
--- deploy.js
+++ deploy.js
@@ -11,13 +11,16 @@ const deployer = new FPR.Deployer(web3)
 
 console.log ("Deploying smart contracts to Ropsten")
 deployer.web3.eth.accounts.wallet.add(account)
+
 deployer.deploy(account).then(addresses => {
+
     const replace = {'_reserve': 'reserve','_conversionRates':'conversionRates', '_sanityRates': 'sanityRates'}
     let replacedaddresses = Object.keys(addresses).map((key) => {
         const n = replace[key]||key
       return {[n]:addresses[key]}})
       var caddresses = replacedaddresses.reduce((a,b) => Object.assign({},a,b))
       var jsonContent = JSON.stringify(caddresses);
+
       fs.writeFile("addresses.json", jsonContent, 'utf8', function (err) {
           if (err) {
               console.log("An error occured while writing JSON Object to File.");
