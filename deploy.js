ca1481e9675486ffd74cd2074e0c12ea6b92a9c0
ssreelekhya@gmail.com <61680234+ssreelekhya@users.noreply.github.com>
2020-07-03 17:28:05 +0800

    changed deploy script

--- deploy.js
+++ deploy.js
@@ -12,21 +12,10 @@ const deployer = new FPR.Deployer(web3)
 console.log ("Deploying smart contracts to Ropsten")
 deployer.web3.eth.accounts.wallet.add(account)
 
-deployer.deploy(account).then(addresses => {
-
-    const replace = {'_reserve': 'reserve','_conversionRates':'conversionRates', '_sanityRates': 'sanityRates'}
-    let replacedaddresses = Object.keys(addresses).map((key) => {
-        const n = replace[key]||key
-      return {[n]:addresses[key]}})
-      var caddresses = replacedaddresses.reduce((a,b) => Object.assign({},a,b))
-      var jsonContent = JSON.stringify(caddresses);
-
-      fs.writeFile("addresses.json", jsonContent, 'utf8', function (err) {
-          if (err) {
-              console.log("An error occured while writing JSON Object to File.");
-              return console.log(err)
-          }
-          console.log("JSON file has been saved.")
-      })
-      console.log("smart contracts are deployed!" + caddresses)
-   })
+deployer.deploy(account).then(res => {
+    console.log("Deploying contracts to Ropsten")
+    fs.writeFileSync("addresses.json",JSON.stringify(
+        {reserve: res.reserve, 
+            conversionRates: res.conversionRates, 
+            sanityRates: res.sanityRates}))
+            console.log("Done!")})
