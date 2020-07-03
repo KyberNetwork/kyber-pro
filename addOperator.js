a096940ee4c18fafd140ca989fba7b3b88b51567
ssreelekhya@gmail.com <61680234+ssreelekhya@users.noreply.github.com>
2020-07-03 12:37:23 +0800

    seperated script files

---
+++ addOperator.js
@@ -0,0 +1,14 @@
+//example to set operator to the conversionrates contract
+require('dotenv').config()
+var FPR = require("kyber-fpr-sdk")
+var Web3 = require("web3")
+var addresses = require("./addresses.json")
+const provider = new Web3.providers.HttpProvider(process.env.ROPSTEN_NODE_URL)
+const web3 = new Web3(provider)
+const account = web3.eth.accounts.privateKeyToAccount(process.env.TESTNET_PRIVATE_KEY)
+const operator = web3.eth.accounts.privateKeyToAccount(process.env.TEST_OPERATOR_PRIVATE_KEY)
+const CRContract = new FPR.ConversionRatesContract(web3, addresses.conversionRates)
+web3.eth.accounts.wallet.add(account)
+//setting operator
+CRContract.addOperator(account, operator.address)
+.then( result => { console.log(result)})
--- addToken.js
+++ addToken.js
@@ -1,9 +1,9 @@
 require('dotenv').config()
+require('dotenv').config()
 var FPR = require("kyber-fpr-sdk")
-var conversionRates = FPR.ConversionRatesContract
+var conversionRates = require("kyber-fpr-sdk/dist/conversion_rates_contract")
 var Web3 = require("web3")
 var addresses = require("./addresses.json")
-
 const tokenInfo = new conversionRates.TokenControlInfo(100000000000000,440000000000000000000n,920000000000000000000n)
 const KTTokenAddress = "0xc376079608C0F17FE06b9e950872666f9c3C3DA4"
 
@@ -13,8 +13,6 @@ const account = web3.eth.accounts.privateKeyToAccount(process.env.TESTNET_PRIVAT
 web3.eth.accounts.wallet.add(account)
 
 const manageReserve = new FPR.Reserve(web3, addresses)
-const CRContract = new FPR.ConversionRatesContract(web3, addresses.conversionRates)
-
 manageReserve.addToken(account, KTTokenAddress, tokenInfo)
    .then( (result) => {
       console.log(result)
