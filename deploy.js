86864dad57161a6946aaf2f4173ffb5d7084b65a
ssreelekhya@gmail.com <61680234+ssreelekhya@users.noreply.github.com>
2020-07-07 15:45:54 +0800

    modify script to take KN address as param

--- deploy.js
+++ deploy.js
@@ -6,14 +6,15 @@ var fs = require("fs");
 const provider = new Web3.providers.HttpProvider(process.env.TESTNET_NODE_URL);
 const web3 = new Web3(provider);
 const account = web3.eth.accounts.privateKeyToAccount(process.env.TESTNET_ADMIN_PRIVATE_KEY);
-
+//@param KNAddress - this is for ropsten testnet. for mainnet use 0x7C66550C9c730B6fdd4C03bc2e73c5462c5F7ACC
+const KNAddress= '0x920B322D4B8BAB34fb6233646F5c87F87e79952b';
 const deployer = new FPR.Deployer(web3);
 
 console.log ("Deploying smart contracts to Ropsten");
 deployer.web3.eth.accounts.wallet.add(account);
 
 (async ()=>{
-    const res = await deployer.deploy(account);
+    const res = await deployer.deploy(account, KNAddress);
     fs.writeFileSync("addresses.json",JSON.stringify(
         {reserve: res.reserve, conversionRates: 
             res.conversionRates, sanityRates:
