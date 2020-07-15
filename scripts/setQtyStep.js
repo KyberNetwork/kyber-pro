5c13a7924e29153224d8b7067e6c352f3fe9c67c
ssreelekhya@gmail.com <61680234+ssreelekhya@users.noreply.github.com>
2020-07-15 18:13:59 +0800

    added additional scripts

---
+++ scripts/setQtyStep.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+var FPR = require("kyber-fpr-sdk");
+var Web3 = require("web3");
+var data = require("./stepFuncdata.json");
+var addresses = require("./addresses.json");
+const KTTokenAddress = "0xc376079608C0F17FE06b9e950872666f9c3C3DA4";
+ 
+const provider = new Web3.providers.HttpProvider(process.env.TESTNET_NODE_URL);
+const web3 = new Web3(provider);
+const operator = web3.eth.accounts.privateKeyToAccount(process.env.TEST_OPERATOR_PRIVATE_KEY);
+const reserveManager = new FPR.Reserve(web3, addresses);
+web3.eth.accounts.wallet.add(operator);
+ 
+const buy = data.buy.map(e=>new FPR.StepFunctionDataPoint(e.x,e.y));
+const sell = data.sell.map(e=>new FPR.StepFunctionDataPoint(e.x,e.y));
+ 
+(async () => {
+   
+   //setQtyStepFunction is a only operator function 
+   console.log("setting quantity step func's");
+   await reserveManager.setQtyStepFunction(operator.address, KTTokenAddress, buy, sell);
+   console.log("done");
+})();
+ 
---
+++ scripts/setValidDuration.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+var FPR = require("kyber-fpr-sdk");
+var Web3 = require("web3");
+var addresses = require("./addresses.json");
+const provider = new Web3.providers.HttpProvider(process.env.TESTNET_NODE_URL)
+const web3 = new Web3(provider)
+const account = web3.eth.accounts.privateKeyToAccount(process.env.TESTNET_ADMIN_PRIVATE_KEY)
+web3.eth.accounts.wallet.add(account);
+const abi = require("kyber-fpr-sdk/abi/ConversionRatesContract.abi.json");
+const crc = new web3.eth.Contract(abi, addresses.conversionRates);
+
+//default gas
+  const gas = 500 * 1000;
+console.log("setting valid duration blocks");
+crc.methods.setValidRateDurationInBlocks(10000 //number of blocks
+    ). send({
+        from: account.address,
+        gas: gas
+    })
+    console.log("done");
+
