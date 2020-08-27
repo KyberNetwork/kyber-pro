abeefdbf2207bd6f14cbdebc63be0a28d5ca4797
sreelekhya <ssreelekhya@gmail.com>
2020-08-27 18:22:07 +0800

    deploy sanity rate

--- scripts/13.deploySrc.js
+++ scripts/13.deploySrc.js
@@ -14,7 +14,7 @@ const provider = new Web3.providers.HttpProvider(process.env.TESTNET_NODE_URL);
 const web3 = new Web3(provider);
 const account = web3.eth.accounts.privateKeyToAccount(process.env.TESTNET_ADMIN_PRIVATE_KEY);
 const deployer = new FPR.Deployer(web3);
-
+const KNAddress = '0x920B322D4B8BAB34fb6233646F5c87F87e79952b';
 console.log ("Deploying sanityrates contract to Ropsten");
 deployer.web3.eth.accounts.wallet.add(account);
 const reserveManager = new FPR.Reserve(web3, addresses);
